2bcf47eadea54f46a116b3a8c08ec894202fd0dd
Tanmay Ikare <ikare.tanmay@gmail.com>
2022-12-02 13:57:38 +0530

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,116 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import ImageTk, Image
+
+import numpy
+#load the trained model to classify sign
+from keras.models import load_model
+model = load_model('my_model1.h5')
+
+#dictionary to label all traffic signs class.
+
+
+def getCalssName(classNo):
+    if   classNo == 0: return 'Speed Limit 20 km/h'
+    elif classNo == 1: return 'Speed Limit 30 km/h'
+    elif classNo == 2: return 'Speed Limit 50 km/h'
+    elif classNo == 3: return 'Speed Limit 60 km/h'
+    elif classNo == 4: return 'Speed Limit 70 km/h'
+    elif classNo == 5: return 'Speed Limit 80 km/h'
+    elif classNo == 6: return 'End of Speed Limit 80 km/h'
+    elif classNo == 7: return 'Speed Limit 100 km/h'
+    elif classNo == 8: return 'Speed Limit 120 km/h'
+    elif classNo == 9: return 'No passing'
+    elif classNo == 10: return 'No passing for vechiles over 3.5 metric tons'
+    elif classNo == 11: return 'Right-of-way at the next intersection'
+    elif classNo == 12: return 'Priority road'
+    elif classNo == 13: return 'Yield'
+    elif classNo == 14: return 'Stop'
+    elif classNo == 15: return 'No vechiles'
+    elif classNo == 16: return 'Vechiles over 3.5 metric tons prohibited'
+    elif classNo == 17: return 'No entry'
+    elif classNo == 18: return 'General caution'
+    elif classNo == 19: return 'Dangerous curve to the left'
+    elif classNo == 20: return 'Dangerous curve to the right'
+    elif classNo == 21: return 'Double curve'
+    elif classNo == 22: return 'Bumpy road'
+    elif classNo == 23: return 'Slippery road'
+    elif classNo == 24: return 'Road narrows on the right'
+    elif classNo == 25: return 'Road work'
+    elif classNo == 26: return 'Traffic signals'
+    elif classNo == 27: return 'Pedestrians'
+    elif classNo == 28: return 'Children crossing'
+    elif classNo == 29: return 'Bicycles crossing'
+    elif classNo == 30: return 'Beware of ice/snow'
+    elif classNo == 31: return 'Wild animals crossing'
+    elif classNo == 32: return 'End of all speed and passing limits'
+    elif classNo == 33: return 'Turn right ahead'
+    elif classNo == 34: return 'Turn left ahead'
+    elif classNo == 35: return 'Ahead only'
+    elif classNo == 36: return 'Go straight or right'
+    elif classNo == 37: return 'Go straight or left'
+    elif classNo == 38: return 'Keep right'
+    elif classNo == 39: return 'Keep left'
+    elif classNo == 40: return 'Roundabout mandatory'
+    elif classNo == 41: return 'End of no passing'
+    elif classNo == 42: return 'End of no passing by vechiles over 3.5 metric tons'
+ 
+
+
+
+
+
+                 
+#initialise GUI
+top=tk.Tk()
+top.geometry('800x600')
+top.title('Traffic sign classification')
+top.configure(background='#CDCDCD')
+
+label=Label(top,background='#CDCDCD', font=('arial',15,'bold'))
+sign_image = Label(top)
+
+def classify(file_path):
+    global label_packed
+    image = Image.open(file_path)
+    image = image.resize((30,30))
+    image = numpy.expand_dims(image, axis=0)
+    image = numpy.array(image)
+    print(image.shape)
+    pred = model.predict([image])
+    # sign = classes[pred[],0]
+    # print(sign)
+    sign = getCalssName(pred.any())
+    label.configure(foreground='#011638', text=sign) 
+   
+
+def show_classify_button(file_path):
+    classify_b=Button(top,text="Classify Image",command=lambda: classify(file_path),padx=10,pady=5)
+    classify_b.configure(background='#364156', foreground='white',font=('arial',10,'bold'))
+    classify_b.place(relx=0.79,rely=0.46)
+
+def upload_image():
+    try:
+        file_path=filedialog.askopenfilename()
+        uploaded=Image.open(file_path)
+        uploaded.thumbnail(((top.winfo_width()/2.25),(top.winfo_height()/2.25)))
+        im=ImageTk.PhotoImage(uploaded)
+        
+        sign_image.configure(image=im)
+        sign_image.image=im
+        label.configure(text='')
+        show_classify_button(file_path)
+    except:
+        pass
+
+upload=Button(top,text="Upload an image",command=upload_image,padx=10,pady=5)
+upload.configure(background='#364156', foreground='white',font=('arial',10,'bold'))
+
+upload.pack(side=BOTTOM,pady=50)
+sign_image.pack(side=BOTTOM,expand=True)
+label.pack(side=BOTTOM,expand=True)
+heading = Label(top, text="Know Your Traffic Sign",pady=20, font=('arial',20,'bold'))
+heading.configure(background='#CDCDCD',foreground='#364156')
+heading.pack()
+top.mainloop()
